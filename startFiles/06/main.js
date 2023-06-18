e07d5f251a9fb8c806dc3cbac58f33a62b628678
Vitali Bedik <vitalya.bedik@gmail.com>
2023-06-18 13:01:02 +0300

    feat: finish file 06-lesson

--- startFiles/06/main.js
+++ startFiles/06/main.js
@@ -5,28 +5,99 @@
 
 //=============================__proto__==================================
 // Для чего нужны прототипы?
+// Наследование
 
 
 
 // Как наследовать свойства из одного объекта в другой?
 
+const student = {
+    name: 'Vitalya',
+    age: 30
+    // __proto__: {} new Object()
+}
+
+
+const mentor = {
+    isMentor: true
+}
+
+mentor.__proto__ = student
+
+// console.log(mentor.__proto__ = student1)
+
+// console.log(student instanceof Object)
+
+// console.log(mentor)
+// console.log(mentor.__proto__.name)
+// console.log(mentor.name)
+// console.log(mentor.age)
+
+
+// Object.create(), Object.hasOwnProperty()
+// const mentor1 = Object.create(student)
+// const mentor2 = Object.create(student, {
+//     isMentor: {
+//         value: true,
+//         writable: true,
+//         enumerable: true,
+//         configurable: true
+//     }
+// });
+// console.log(mentor1.name)
+// console.log(mentor2)
 
 
 // Чем может быть равен __proto__?
+// Равно объекту либо null
 
 
 
 // У кого есть __proto__?
+// есть у всех объектов
 
+// const number = new Number()
+// const string = new String()
+// const obj = new Object()
+// const array = new Array()
+// const array = new Function()
+// const date = new Date()
+
+const man1 = {}  // new Object()
+const man2 = {}  // new Object()
+
+const a1 = 1
+
+// console.log(man1.__proto__)
+// console.log(a1.__proto__)
 
 
 //=============================prototype==================================
-// функции-конструкторы в JS встроенные какие знаете?
+// функции-конструкторы в JS встроенные какие знаете?  ++
 
+
+// function Car(brand) {
+//     this.brand = brand
+// }
+//
+// const isPremiumCar = {
+//     isPremium: true,
+//     getBrand() {
+//         return this.brand
+//     }
+// }
+//
+// Car.prototype = isPremiumCar
+//
+// const bmw = new Car('bmw')
+
+// console.log(bmw.isPremium)
+// console.log(bmw.getBrand())
 
 
 // У кого есть свойство prototype?
-
+// __proto__ у всех объектов
+// prototype у всех функций и классов
 
 
 // Какая связь между __proto__ и prototype?
@@ -47,20 +118,45 @@
 
 
 // Задание свойства в прототип для функции-конструктора
+function Car(brand) {
+    this.brand = brand
+}
+Car.prototype.sayHi = function () {
+    return 'Привет'
+}
 
+Car.prototype.incubator = 100200
+
+const bmw = new Car('bmw')
+//
+// console.log(bmw.incubator)
+//
+// console.log(bmw.__proto__)
 
 
 // Задание метода в прототип для функции-конструктора
 
-
-
+// console.log(bmw.sayHi())
 // Как работает this для прототипов?
 
+Number.prototype.anyMethod = function () {
+    return 'anyMethod'
+}
+
+String.prototype.customRepeat = function (str) {
+    return this + str
+}
+
+// console.log((1).anyMethod())
+// console.log('12431234'.customRepeat('samurai'))
+// console.log('incub'.customRepeat('samurai'))
 
 
 //=============================Создание своих методов для примитивов================================
 // Откуда у примитивов взялись методы?
 // Расскажи на примере (1).toString()
+
+// console.log((1).toString())
 
 
 
@@ -175,8 +271,12 @@ const frontData = {
 }
 
 // как из данного объекта сделать JSON?
-
-
+const jsonFrontData = JSON.stringify(frontData)
+// console.log(frontData)
+// console.log(jsonFrontData)
+//
+// console.log(typeof frontData)
+// console.log(typeof jsonFrontData)
 
 // В чем отличие данных в JSON от обычного объекта?
 
@@ -187,9 +287,8 @@ const frontData = {
 
 // Как теперь серверу превратить этот JSON обратно в объект?
 
-
+const jsonBackData = JSON.parse(jsonFrontData)
 // console.log(jsonBackData)
-
 
 
 
@@ -198,16 +297,99 @@ const frontData = {
 
 // Для чего нужна данная конструкция и когда используется?
 
+// Нужно чтобы код работал дальше и программа не упала
 
 // Базовая запись
+// try {
+//
+// } catch(error) {
+//
+// } finally {
+//
+// }
+
 
 
 
 // Как обработать ошибку?
 
+// const a = 1
+//
+//     const b = a + c
+// try {
+//     console.log(b)
+// } catch(error) {
+//     console.log(error)
+// }
+//
+// console.log('123')
+
+// ==================================================
+
+// const a = 1
+//
+// try {
+//     console.log('Я в try')
+//     const b = a + c
+//     console.log(b)
+//     console.log('Я все еще try')
+// } catch(error) {
+//     // console.log(error.name)
+//     // console.log(error.message)
+//     // console.log(error.stack)
+//     console.log('Я в catch')
+// } finally {
+//     console.log('Я finally')
+// }
+//
+// console.log('Я вне конструкции try catch')
+
+//
+// const a = 1
+//
+// try {
+//     console.log('Я в try')
+//     const b = a + c
+//     console.log(b)
+//     console.log('Я все еще try')
+// } catch(error) {
+//     // console.log(error.name)
+//     // console.log(error.message)
+//     // console.log(error.stack)
+//     console.log('Я в catch')
+// } finally {
+//     console.log('Я finally')
+// }
+//
+// console.log('Я вне конструкции try catch')
+
 
 
 // Что делает блок try?
+
+// const a = 1
+//
+// try {
+//     const a1 = 10
+//
+//     const b1 = a1 + c1
+//
+//     try {
+//         console.log('Я в try')
+//         const b = a + c
+//         console.log(b)
+//         console.log('Я все еще try')
+//     } catch ({name, message, stack}) {
+//         console.log(name)
+//         console.log(message)
+//         console.log(stack)
+//         console.log('Я в catch')
+//     }
+// } catch (e) {
+//     console.log(e)
+// }
+
+// console.log('Я вне конструкции try catch')
 
 
 
@@ -226,9 +408,30 @@ const frontData = {
 // Что делает блок finally?
 
 
+// try {
+//     console.log('Запускаем крутилку')
+//     console.log('Делаем запрос на сервер -> fetch/axios')
+//     console.log('сетаем данные / диспатчим нашу санку')
+// } catch (e) {
+//     console.log('обрабатываем ошибку, например показываем popup')
+// } finally {
+//     console.log('Убираем крутилку')
+// }
+
+
 
 // Как создать пользовательскую ошибку?
 
+// try {
+//     console.log('Запускаем крутилку')
+//     console.log('Делаем запрос на сервер -> fetch/axios')
+//     console.log('сетаем данные / диспатчим нашу санку ???')
+//     throw new Error('СВОЯ ОШИБКА КАСТОМНАЯ')
+// } catch (e) {
+//     console.log(e)
+// } finally {
+//     console.log('Убираем крутилку')
+// }
 
 
 
