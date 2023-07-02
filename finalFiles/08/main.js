ae2907db3b4beb3cc2581c09e3130c0d8e05d681
Vitali Bedik <vitalya.bedik@gmail.com>
2023-07-02 12:53:37 +0300

    feat: start finish 08-lesson

--- finalFiles/08/main.js
+++ finalFiles/08/main.js
@@ -1,9 +1,24 @@
 //============================== Промисы ================================
 //============================== Базовая информация по HTTP и клиент-серверной архитектуре ===
-
+// Что такое HTTP?
+// Что такое протокол?
+// Для чего предназначен HTTP протокол?
+// Что значит прикладного уровня?
+// Что из себя представляет клиент-серверная архитектура?
+// Кто является инициатором запроса в клиент-серверной архитектуре?
+// Что делает сервер на клиентский запрос?
+// Что в этом ответе нас должно волновать (в первую очередь)?
+// Для чего статус код ответа?
+// Как описать серверу что мы от него хотим при запросе?
+// Какие запросы позволяет отправлять fetch?
+// Какую технологию поддерживает fetch?
+// Что такое AJAX и для чего?
+// Какие приложения позволяет делать технология AJAX?
 
 //============================== Метод fetch (отправим запрос на сервер) ====================
-
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
 
 //============================== Где появляются промисы у нас в проекте? ====================
 
@@ -11,26 +26,152 @@
 //============================== Синхронный и Асинхронный код ================================
 
 
-
 //============================== Эволюция развития работы с асинхронным кодом ===============
-
+// коллбэки -> промисы
 
 
 //============================== Что такое callback? =========================================
 
+//
+// function startEngine(callback) {
+//     return callback()
+// }
+//
+// function start() {
+//     return 'Go'
+// }
+//
+// console.log(startEngine(start))
+
+
+
+// function startEngine(callback) {
+//    setTimeout(() => callback(), 6000)
+//     return '1234'
+// }
+//
+// function start() {
+//     return 'Go'
+// }
+//
+// console.log(startEngine(start))
+
 
 
 //============================== Что такое промис и как его создать? ==========================
+// Промис это объект
+// new Promise -> принимает callback (executor)
+
+// executor принимает 2 коллбэк функции
 
 
+// new Promise((resolve, reject) => {
+//     resolve(1)
+//     // reject('error')
+// })
+
+
+
+// const myPromise = new Promise((resolve, reject) => {
+//     console.log(123)
+//     // resolve(1)
+//     console.log(234)
+//     // reject('error')
+// })
+//
+// console.log(myPromise)
+
+// const promise = {
+//     status: pending | fulfilled           | rejected
+//     value: undefined | value из resolve  | value из rejected
+// }
 
 //============================== Методы промиса ===============================================
+// then, catch, finally
 
+// then
+/*
+then - срабатывает когда запрос завершен успешно или зарезолвился
+Какие параметры -> 2 коллбэк функции (первая для успешного выполнения, вторая - неуспешного)
+ */
+
+// then((resolved) => {}, (rejected) => {})
+
+//
+// const myPromise = new Promise((resolve, reject) => {
+//     // console.log(123)
+//     // resolve(1)
+//     // console.log(234)
+//     // reject('error')
+//     // setTimeout(() => resolve('resolve'), 1000)
+//     setTimeout(() => reject('reject'), 1000)
+// })
+//
+// myPromise.then((res) => console.log(res))
+
+
+// catch
+/*
+catch - срабатывает когда запрос неуспешен (зареджектился)
+Какие параметры -> коллбэк функцию
+ */
+
+// const myPromise = new Promise((resolve, reject) => {
+//     // console.log(123)
+//     // resolve(1)
+//     // console.log(234)
+//     // reject('error')
+//     // setTimeout(() => resolve('resolve'), 1000)
+//     setTimeout(() => reject('reject'), 1000)
+// })
+
+// const p1 = myPromise.then((res) => console.log(res))
+//
+// const p2 = p1.catch((res) => console.log(res))
+
+// const p3 = p2.catch((res) => console.log(res))
+//
+// console.log(p1 === p2)
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+//     .catch(json => console.log('Ошибка'))
+
+
+// finally
+/*
+finally - срабатывает вне зависимости от того зарезолвился или зареджектился промис
+Какие параметры -> коллбэк функцию
+
+!!! Ничего не принимает и ничего не возвращает
+ */
+
+// finally(() => {})
 
 
 //============================== Цепочка промисов =============================================
-
-
+// const myPromise = new Promise((resolve, reject) => {
+//
+//     // resolve(2)
+//     reject('Все пропало')
+// })
+//     .finally(() => console.log('Просто выведу в консоль'))
+//     .then((data1) => console.log(data1 + 3))
+//     .then((data2) => console.log(data2 * 2))
+//     .catch((err) => console.log(err))
+//     // .catch((err) => new Error('Еще ошибка'))
+//     .catch((err) => console.log(err))
+//     .then(() => {
+//         return new Promise((resolve) => {
+//             resolve('Я новый резолв')
+//         })
+//     })
+//     .then((data4) => console.log(data4))
+//     .catch((err) => console.log(err))
+//
+//
+// console.log(myPromise)
 
 
 // ============================================= Задачи на отработку =====================================================
@@ -58,6 +199,13 @@ const p = new Promise((resolve, reject) => {
 !!!!
  */
 
+
+// const p = new Promise((resolve, reject) => {
+//     reject('The Fails!')
+// })
+//     .catch(error => console.log(error))
+//     .then(error => console.log(error))
+
 // ============================================= Задача 3 =====================================================
 /*
 !!!!
@@ -68,6 +216,7 @@ const p = new Promise((resolve, reject) => {
   .catch(error => console.log(error.message))
 !!!!
  */
+
 
 // ============================================= Задача 4 =====================================================
 /*
@@ -84,6 +233,18 @@ new Promise((resolve, reject) => {
   .catch(error => console.log(error.message))
 !!!!
  */
+
+// new Promise((resolve, reject) => {
+//     resolve('Success!')
+// })
+//     .then(() => {
+//        console.log(Error('Oh noes!'))
+//     })
+//     .catch(error => {
+//         console.log(error)
+//         return "actually, that worked"
+//     })
+//     .catch(error => console.log(error.message))
 
 // ============================================= Задача 5 =====================================================
 /*
