849c9431655bff33e45e44d91807d0c50a127c1d
Vitali Bedik <vitalya.bedik@gmail.com>
2023-07-09 14:22:59 +0300

    feat: finish 09-lesson

--- finalFiles/09/main.js
+++ finalFiles/09/main.js
@@ -21,51 +21,101 @@
 //         console.log(data);
 //     })
 
+
+// const p = Promise.resolve(4);
+//
+// p
+//     .then(data => {
+//         console.log(data);                                             // 4
+//         throw new Error('Введены неправильные данные');                // создаем ошибку и прокидываем дальше
+//     })
+//
+//     .catch(err => {
+//         console.log('err');                                            // err
+//
+//         return new Error('Ошибка');
+//     })
+//     .then((data) => {
+//         console.log(data);                                              // ok
+//     })
+//     .catch(err => {
+//         console.log(err);                                            // err
+//     })
+
+
 //============================== Задача 2 ================================
 // Что выведет в консоль и почему?
 // Promise.resolve(1)
-//     .then(data => console.log(data))
+//     .then(data => console.log(data))                     // 1
 //
 //     .then(data => {
-//         console.log(data);
+//         console.log(data);                               // undefined
 //         return 2;
 //     })
 //
 //     .then(data => {
-//         console.log(data);
-//         throw new Error('Api Error');
+//         console.log(data);                              // 2
+//         throw new Error('Api Error');                   // создаем ошибку и пробрасываем
 //         return 1;
 //     })
 //
-//     .then(data => console.log('Hello'))
+//     .then(data => console.log('Hello'))                  // пропускаем
 //
 //     .catch(error => {
-//         console.log(error.message);
+//         console.log(error.message);                     //  'Api Error'
 //         return 3;
 //     })
 //
 //     .then(data => {
-//         console.log(data);
+//         console.log(data);                             // undefined -> !!! 3
 //         return 4;
 //     })
 //
 //     .finally(data => {
-//         console.log(data);
+//         console.log(data);                             // undefined
 //         return 5;
 //     })
 //
-//     .then(data => console.log(data))
-
+//     .then(data => console.log(data))                   // undefined -> !!! 4
 
 
 //============================== Promise API ================================
 // Какие статические методы есть у класса Promise?
-
+// resolve reject all any allSettled race
 
 //============================== Promise.all ================================
 // Создай 3 промиса, которые зарезолвятся через 4, 1, 3 сек
 
+// const pr1 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Я первый промис'), 4000)
+// })
+//
+// const pr2 = new Promise((resolve, reject) => {
+//     // setTimeout(() => resolve('Я второй промис'), 1000)
+//     reject('Ошибка')
+// })
+//
+// const pr3 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Я третий промис'), 3000)
+// })
+
+// const pr4 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Я четвертый промис'), 500)
+// })
+//
+// const pr5 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Я пятый промис'), 2000)
+// })
+//
+// pr1.then((res) => console.log(res))
+// pr2.then((res) => console.log(res))
+// pr3.then((res) => console.log(res))
+
 // Для чего нужен метод all?
+
+// const res = Promise.all([pr1, pr2, pr3, pr4, pr5])
+//
+// res.then(res => console.log(res)).catch(e => console.log(e))
 
 
 // Что принимает в качестве параметров метод all?
@@ -83,13 +133,28 @@
 // Как теперь обработать promise?
 
 
-
 // Что будет, если хотя бы один promise будет с ошибкой?
-
 
 
 //============================== Promise.race ================================
 // Для аналогичных промисов примени метод race
+
+// const pr1 = new Promise((resolve, reject) => {
+//     // setTimeout(() => resolve('Я первый промис'), 4000)
+//     reject('Я первый промис')
+// })
+//
+// const pr2 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Я второй промис'), 1000)
+// })
+// const pr3 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Я третий промис'), 3000)
+// })
+//
+// const res = Promise.race([pr1, pr2, pr3])
+//
+// console.log(res)
+// res.then(data => console.log(data))
 
 // Что возвращает race?
 
@@ -100,24 +165,118 @@
 // Разница между all race?
 
 
-
 //============================== Promise.any ================================
 // Как работает any?
-
 
 
 // Что возвращает any?
 
 
+// const pr1 = new Promise((resolve, reject) => {
+//     setTimeout(() => reject('Я первый промис'), 4000)
+//     // reject('Я первый промис')
+// })
+//
+// const pr2 = new Promise((resolve, reject) => {
+//     // setTimeout(() => resolve('Я второй промис'), 1000)
+//     setTimeout(() => reject('Я второй промис'), 1000)
+// })
+//
+// const pr3 = new Promise((resolve, reject) => {
+//     setTimeout(() => reject('Я третий промис'), 3000)
+// })
+//
+// const res = Promise.any([pr1, pr2, pr3])
+//
+// res.then(data => console.log(data))
 
 //============================== Promise.allSettled ================================
 // Как работает allSettled?
 
+const pr1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Я первый промис'), 4000)
+    // reject('Я первый промис')
+})
+
+const pr2 = new Promise((resolve, reject) => {
+    // setTimeout(() => resolve('Я второй промис'), 1000)
+    setTimeout(() => reject('Я второй промис'), 1000)
+})
+
+const pr3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Я третий промис'), 3000)
+})
+
+const res = Promise.allSettled([pr1, pr2, pr3])
+
+res.then(data => console.log(data))
+
 // Что возвращает allSettled?
 
 
-
 //============================== async/await ================================
+// const foo1 = () => {
+//     return new Promise(res => {
+//        res(123)
+//     })
+// }
+//
+// console.log(foo1())
+
+// arrow
+// const foo = async () => {
+//     return 123
+// }
+//
+// // fd
+// async function foo1() {
+//     return 123
+// }
+//
+// // fe
+// const foo2 = async function() {
+//     return 123
+// }
+
+// console.log(foo())
+// console.log(foo1())
+// console.log(foo2())
+
+
+//============================== прочитать ответ ================================
+// const foo = async () => {
+//     return 123
+// }
+//
+// const promise = foo()
+//
+// console.log(promise)
+// foo()
+//     .then(res => console.log(res))
+
+
+// const timerPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(123)
+//         }, 2000)
+//     })
+// }
+//
+// const asyncFn = async () => {
+//     console.log('Timer starts')
+//     const resAw = await timerPromise()
+//
+//     console.log(resAw)
+//     console.log('Timer ended')
+// }
+//
+// console.log('start')
+// asyncFn()
+//
+// console.log('end')
+// asyncFn().then(res => console.log(res))
+
 
 //============================== Ключевое слово async ================================
 // Что позволяет делать?
@@ -170,6 +329,57 @@ console.log('promise', promise)
 12. Попробуй сломать URL -> посмотри что будет
  */
 
+// fetch
+// const USERS_URL = `https://jsonplaceholder.typicode.com/users`
+// const TODOS_URL = `https://jsonplaceholder.typicode.com/todos`
+//
+// fetch(USERS_URL)
+//     .then(response => response.json())
+//     .then(users => {
+//         // console.log('users', users)
+//         const firstUserId = users[0].id
+//         // console.log('firstUserId', firstUserId)
+//
+//         fetch(`${TODOS_URL}?userId=${firstUserId}`)
+//             .then(response => response.json())
+//             .then(todos => {
+//                 console.log(todos)
+//             })
+//     })
+//     .catch(error => {
+//         console.log('error', error)
+// //     })
+
+// async/await
+// const USERS_URL = `https://jsonplaceholder.typicode.com/u`
+// const TODOS_URL = `https://jsonplaceholder.typicode.com/todos`
+//
+// const getTodosWithUserData = async () => {
+//     console.log('включили крутилку')
+//     try {
+//         const usersResponse = await fetch(USERS_URL)                              // then
+//         if (!usersResponse.ok) {
+//             throw new Error('Ошибка в получении данных о пользователях!')
+//         }
+//         const users = await usersResponse.json()                                       // then
+//         const firstUserId = users[0].id
+//
+//         const todosResponse = await fetch(`${TODOS_URL}?usId=${firstUserId}`)  // then
+//         if (!todosResponse.ok) {
+//             throw new Error('Ошибка в получении данных о задачах!')
+//         }
+//         const todosFirstUser = await todosResponse.json()                             // then
+//
+//         return todosFirstUser
+//     } catch (error) {
+//         console.log(error.message + 'я в catch')
+//     } finally {
+//         console.log('выключили крутилку')
+//     }
+// }
+//
+// getTodosWithUserData().then(res => console.log(res))
+
 
 //============================== Задачи ================================
 
@@ -202,6 +412,24 @@ createNewPost();
  */
 
 
+// const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+// let isLoading = false;
+//
+// const createNewPost = async () => {
+//     try {
+//         isLoading = true;
+//         const response = await fetch(POSTS_URL, {method: 'POST'})
+//         const result = await response.json()
+//
+//         return result
+//     } catch (error) {
+//         console.log(error)
+//     } finally {
+//         isLoading = false;
+//     }
+// }
+//
+// createNewPost().then(res => console.log(res))
 
 //============================== Задача 2 ================================
 /*
