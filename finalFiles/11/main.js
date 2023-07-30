a9a88c11fd3ccad58e471d8d9cd6458cdfb5a633
Vitali Bedik <vitalya.bedik@gmail.com>
2023-07-30 13:34:51 +0300

    feat: finish 11-lesson

--- finalFiles/11/main.js
+++ finalFiles/11/main.js
@@ -4,6 +4,8 @@
 // Что такое ООП?
 
 // Способы создания объектов в ООП
+// функция-конструктор и класса
+
 
 //============================== Создание объектов с помощью функции-конструктора =========
 /*
@@ -15,9 +17,32 @@
 и путем вызова метода getPetName
  */
 
+// function Animal(name) {
+//     this.name = name
+// }
+//
+// Animal.prototype.getPetName = function() {
+//     return this.name
+// }
+//
+// const cat = new Animal('кот')
+// const dog = new Animal('собака')
+//
+// console.log(cat)
+// console.log(cat.getPetName())
+// console.log(dog.getPetName())
+
 
 //============================== Создание объектов с помощью класса =========
 // Как создать класс и какие основные блоки у него есть?
+// class MyClassName {
+//     constructor() {
+//     }
+//
+//     anyMethod() {
+//
+//     }
+// }
 
 
 // Что такое конструктор и для чего он нужен?
@@ -29,19 +54,57 @@
 
 // Чему равен typeof класса?
 
+// console.log(typeof MyClassName)   // function
+
+
 
 // Как создать свойство в классе?
-
-
-// Как вызывать созданный класс?
+//
+// class MyClassName {
+//     constructor(name) {
+//         this.name = name
+//     }
+//
+//     anyMethod() {
+//         console.log(this.name)
+//     }
+// }
+//
+//
+// // Как вызывать созданный класс?
+//
+// const vitalya = new MyClassName('vitalya')
+//
+// console.log(vitalya)
+// vitalya.anyMethod()
 
 
 // Перепиши пример выше с помощью класса
 
+// class Animal {
+//     constructor(name) {
+//         this.name = name
+//     }
+//
+//     getPetName() {
+//         return this.name
+//     }
+// }
+//
+// const cat = new Animal('кот')
+// const dog = new Animal('собака')
+//
+// console.log(cat)
+// console.log(cat.getPetName())
+// console.log(dog.getPetName())
 
 //============================== Принципы ООП ======================================
 
 // Какие есть принципы ООП?
+// Инкапсуляция (incapsulation)
+// Полиморфизм (polymorphism)
+// Абстракция (abstraction)
+// Наследование (inheritance)
 
 //============================== Наследование ======================================
 /*
@@ -53,10 +116,25 @@
  */
 
 
+// class Human {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//     }
+//
+//     sayHello() {
+//         console.log(`Всем привет меня зовут ${this.name}, мне ${this.age}`)
+//     }
+// }
+//
+// const person = new Human('vitali', 29)
+// console.log(person)
+// person.sayHello()
+
 
 //============================== Ключевое слово super ======================================
 
-// Для чего нужен ключевое слово super?
+// Для чего нужно ключевое слово super?
 
 // super в constructor
 
@@ -86,11 +164,54 @@
  */
 
 
-
+// class Human {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//     }
+//
+//     sayHello() {
+//         return (`Всем привет меня зовут ${this.name}, мне ${this.age}`)
+//     }
+// }
+//
+// const person = new Human('vitali', 29)
+// console.log(person)
+// person.sayHello()
+//
+// class Programmer extends Human {
+//     constructor(name, age, isProgrammer) {
+//        super(name, age)
+//         this.isProgrammer = isProgrammer
+//     }
+//
+//     // sayHello() {
+//     //     console.log('Programmer sayHello')
+//     // }
+//
+//     sayHelloProgrammer() {
+//         return super.sayHello() + ', я программист'
+//     }
+// }
+//
+// const programmer = new Programmer('ilya', '23', true)
+// console.log(programmer)
+//
+// programmer.sayHello()
+// console.log(programmer.sayHelloProgrammer())
+//
+// class MyClass {
+//
+// }
 
 
 // Как проверить принадлежит ли наш класс Programmer классу Human?
 
+// console.log(programmer instanceof Programmer)
+// console.log(programmer instanceof MyClass)
+
+// console.log(programmer instanceof Object)
+// console.log(programmer instanceof Human)
 
 
 
@@ -107,6 +228,50 @@
 
 Вывести в консоль все
  */
+
+// class Developer {
+//     #salary
+//
+//     constructor(name, programmingLanguage) {
+//         this.name = name
+//         this.programmingLanguage = programmingLanguage
+//         this.#salary = 1000
+//     }
+//
+//     #anyMethod() {
+//         return 100
+//     }
+//     startCoding() {
+//         console.log(`${this.name} начинает писать код`)
+//     }
+//     printProgrammingLanguage(){
+//         console.log(`Язык программирования: ${this.programmingLanguage}`)
+//     }
+//     showSalary() {
+//         return this.#salary + this.#anyMethod()
+//     }
+// }
+//
+// const developer1 = new Developer('Кирилл', 'python')
+// console.log(developer1.showSalary())
+//
+// // console.log(developer1.#anyMethod())
+// // developer1.startCoding()
+// // developer1.printProgrammingLanguage()
+//
+// class JuniorDeveloper extends Developer {
+//     constructor(name, programmingLanguage, webDevelopmentSkills) {
+//         super(name, programmingLanguage);
+//         this.webDevelopmentSkills = webDevelopmentSkills
+//     }
+// }
+//
+// const junior1 = new JuniorDeveloper('Airat', 'pascal', ['OOP', 'React', 'VB'])
+//
+// // console.log(junior1.#salary)
+// junior1.startCoding()
+// junior1.printProgrammingLanguage()
+
 
 
 /*
@@ -143,11 +308,119 @@
 //============================== Геттеры/Сеттеры ======================================
 // Что такое геттеры и сеттеры?
 
+// class Developer {
+//     #salary
+//
+//     constructor(name, programmingLanguage) {
+//         this.name = name
+//         this.programmingLanguage = programmingLanguage
+//         this.#salary = 1000
+//     }
+//
+//     #startCoding() {
+//         return (`${this.name} начинает писать код`)
+//     }
+//     printProgrammingLanguage(){
+//         console.log(`Язык программирования: ${this.programmingLanguage}`)
+//     }
+//
+//     // get salary() {
+//     //     return this.#startCoding()
+//     // }
+//
+//     get salary() {
+//         console.log('Отрабатывает геттер')
+//         return this.#salary
+//     }
+//
+//     set salary(salary) {
+//         console.log('Отрабатывает сеттер')
+//         if (salary < 400) {
+//             this.#salary = 'Я иду на другой собес'
+//             return
+//         }
+//         this.#salary = salary
+//     }
+// }
+//
+// const developer1 = new Developer('Кирилл', 'python')
+//
+// console.log(developer1.salary)
+//  developer1.salary = 100
+// console.log(developer1.salary)
+
+
+// console.log(developer1.showSalary())
+//
+// console.log(developer1.#anyMethod())
+// developer1.startCoding()
+// developer1.printProgrammingLanguage()
+
+// class JuniorDeveloper extends Developer {
+//     constructor(name, programmingLanguage, webDevelopmentSkills) {
+//         super(name, programmingLanguage);
+//         this.webDevelopmentSkills = webDevelopmentSkills
+//     }
+// }
+//
+// const junior1 = new JuniorDeveloper('Airat', 'pascal', ['OOP', 'React', 'VB'])
+
+// console.log(junior1.salary)
+
+
+
 
 
 //============================== Полиморфизм ======================================
 // Что такое Полиморфизм?
 
+// class Figure {
+//     constructor() {
+//
+//     }
+//
+//     calculateArea() {}
+// }
+//
+// class Circle extends Figure {
+//     constructor() {
+//         super();
+//     }
+//
+//     calculateArea() {
+//         return 'Площадь круга'
+//     }
+// }
+//
+// class Rectangle extends Figure {
+//     constructor() {
+//         super();
+//     }
+//
+//     calculateArea() {
+//         return 'Площадь треугольника'
+//     }
+// }
+//
+// class Square extends Figure {
+//     constructor() {
+//         super();
+//     }
+//
+//     calculateArea() {
+//         return 'Площадь квадрата'
+//     }
+// }
+//
+// const circle = new Circle()
+// const rectangle = new Rectangle()
+// const square = new Square()
+//
+//
+// const arr = [circle, rectangle, square]
+// arr.forEach((el) => {
+//     console.log(el.calculateArea())
+// })
 
 
 //============================== Абстракция ======================================
@@ -160,11 +433,113 @@
 
 // Что такое статическое свойства или метод и как его создать
 
+// class Developer {
+//     // age = 15             // публичное public       -> наследуется
+//     // _age = 10            // защищенное protected   -> наследуется
+//     // #age = 20            // приватное private      -> не наследуется
+//     // static age = 30      // статическое static     -> наследуется
+//
+//     constructor(name, programmingLanguage) {
+//         this.name = name
+//         this.programmingLanguage = programmingLanguage
+//     }
+//
+//     static printProgrammingLanguage(){
+//         console.log(`Язык программирования: ${this.programmingLanguage}`)
+//     }
+// }
+//
+// const developer1 = new Developer('Кирилл', 'python')
+//
+// class JuniorDeveloper extends Developer {
+//     constructor(name, programmingLanguage) {
+//         super(name, programmingLanguage);
+//     }
+// }
+//
+// const junior1 = new JuniorDeveloper('vasya', 'pascal')
+//
+// console.log(JuniorDeveloper.printProgrammingLanguage())
+// // console.log(JuniorDeveloper.age)
+
 
 // пример использования static для задания начальных значений
+
+
+// class Developer {
+//     static isDeveloper(developer) {
+//         return developer instanceof Developer
+//     }
+//
+//     static initialParams = {
+//         name: 'Kirill',
+//         programmingLanguage: 'Python',
+//     }
+//
+//     constructor(name, programmingLanguage) {
+//         this.name = name || Developer.initialParams.name
+//         this.programmingLanguage = programmingLanguage || Developer.initialParams.programmingLanguage
+//     }
+//
+//     printProgrammingLanguage(){
+//         console.log(`Язык программирования: ${this.programmingLanguage}`)
+//     }
+// }
+//
+// // const developer1 = new Developer('vitalya', 'js')
+// const developer2 = new Developer()
+// console.log(developer2)
+//
+// Developer.initialParams.name = 'vasya'
+// const developer3 = new Developer()
+// console.log(developer3)
+
+// const isDeveloper = Developer.isDeveloper(developer1)
+//
+// console.log(isDeveloper)
+
+
+
 
 
 
 //============================== Примеси ======================================
 
 // Для чего придумали примеси? Как реализовать?
+
+// class Developer {
+//     constructor(name, programmingLanguage) {
+//         this.name = name
+//         this.programmingLanguage = programmingLanguage
+//     }
+//
+//     printProgrammingLanguage(){
+//         console.log(`Язык программирования: ${this.programmingLanguage}`)
+//     }
+// }
+//
+//
+// const react = {
+//     technology: 'react',
+//     render() {
+//         return('рисую разметку')
+//     }
+// }
+//
+// const angular = {
+//     stack: 'angular',
+// }
+//
+// const python = {
+//     programmingLanguage: 'python',
+//     ml() {
+//         return ('создаю ИИ')
+//     }
+// }
+//
+// Object.assign(Developer.prototype, react, angular, python)
+// const developer1 = new Developer('vitalya', 'js')
+//
+// console.log(developer1.programmingLanguage)
+// console.log(developer1.ml())
+// console.log(developer1.render())
